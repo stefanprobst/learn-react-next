9725b8b6e6f60e56812eb0753dcf10b52ddd9f77
Stefan Probst <stefanprobst@posteo.de>
2024-03-20 13:24:34 +0100

    feat: handle filter state via url search params

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,12 @@ interface IndexPageProps {
 }
 
 export default function IndexPage(props: IndexPageProps) {
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const { searchParams } = props;
 
 	return (
 		<main className="mx-auto grid w-full max-w-screen-md content-start gap-8 p-8">
 			<h1>StarWars Search</h1>
-			<Search />
+			<Search searchParams={searchParams} />
 		</main>
 	);
 }
--- components/search.tsx
+++ components/search.tsx
@@ -1,5 +1,107 @@
 "use client";
 
-export function Search() {
-	return null;
+import { useRouter } from "next/navigation";
+import { type FormEvent, type ReactNode, useEffect, useState } from "react";
+import { z } from "zod";
+
+const searchParamsSchema = z.object({
+	q: z.string().min(1).optional().default("").catch(""),
+});
+
+interface SearchResult {
+	name: string;
+	url: string;
+}
+
+interface SearchProps {
+	searchParams: Record<string, Array<string> | string>;
+}
+
+export function Search(props: SearchProps): ReactNode {
+	const { searchParams } = props;
+
+	const filters = searchParamsSchema.parse(searchParams);
+
+	const [searchResults, setSearchResults] = useState<Array<SearchResult>>([]);
+
+	useEffect(() => {
+		let isCanceled = false;
+
+		async function fetchData(searchTerm: string) {
+			const response = await fetch(` https://swapi.dev/api/people/?search=${searchTerm}`);
+
+			if (response.ok) {
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+				const data = await response.json();
+				if (!isCanceled) {
+					// eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+					setSearchResults(data.results);
+				}
+			}
+		}
+
+		void fetchData(filters.q);
+
+		return () => {
+			isCanceled = true;
+		};
+	}, [filters.q]);
+
+	useEffect(() => {
+		const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+		function onChangeColorScheme() {
+			alert("COLOR SCHEME CHANGED");
+		}
+
+		mediaQuery.addEventListener("change", onChangeColorScheme);
+
+		return () => {
+			mediaQuery.removeEventListener("change", onChangeColorScheme);
+		};
+	}, []);
+
+	const router = useRouter();
+
+	function onSubmit(event: FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+
+		const formData = new FormData(event.currentTarget);
+
+		// @ts-expect-error No file inputs in form.
+		const newSearchParams = new URLSearchParams(formData);
+
+		router.push("?" + String(newSearchParams));
+	}
+
+	return (
+		<section>
+			<form onSubmit={onSubmit}>
+				<label>
+					<div>Search term</div>
+					<input
+						className="rounded-md border border-black px-3 py-1 shadow"
+						defaultValue={filters.q}
+						name="q"
+					/>
+				</label>
+
+				<button type="submit">Submit</button>
+			</form>
+
+			<div>
+				<ul role="list">
+					{searchResults.map((searchResult) => {
+						return (
+							<li key={searchResult.url}>
+								<article>
+									<span>{searchResult.name}</span>
+								</article>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</section>
+	);
 }
